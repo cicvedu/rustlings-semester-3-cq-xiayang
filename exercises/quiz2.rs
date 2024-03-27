382c316963855206ce9bf3d2d399d09fc24e1e47
cq-xiayang <344164711@qq.com>
2024-03-27 23:33:34 +0800

    update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,11 +32,16 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<&str> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<&str> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Append(n)  => string + "bar",
+                Command::Trim => string.trim(),
+                Command::Uppercase => string.to_uppercase(),
+            }
         }
         output
     }
@@ -45,7 +50,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
